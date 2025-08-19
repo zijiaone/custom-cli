3bf81428ab9bae597daed628b6751b770f3faa8d
zijiawen <zijialoveplay@qq.com>
2025-08-19 10:36:30 +0800

    feat: add dependencies

--- bin/zj-cli.js
+++ bin/zj-cli.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
 
-// 显示欢迎信息
-console.log('\n=== ZJ-CLI 脚手架工具 ===\n');
+const { program } = require('commander');
+const chalk = require('chalk');
+const pkg = require('../package.json');
+
+// 设置版本号
+program.version(pkg.version, '-v, --version');
+
+// 解析命令行参数
+program.parse(process.argv);
+
+// 如果没有提供任何命令，显示帮助信息
+if (!process.argv.slice(2).length) {
+  console.log(chalk.bold.blue('\n 🚀 Welcom to use ZJ-CLI \n'));
+  program.outputHelp((txt) => chalk.cyan(txt));
+  console.log('\n run ' + chalk.green('zj-cli init') + chalk.gray(' - Initialize a new project \n'));
+}
