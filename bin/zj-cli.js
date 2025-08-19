0c7a57d08f927fc908203299095940d203b12bf4
zijiawen <zijialoveplay@qq.com>
2025-08-19 15:11:36 +0800

    feat: add helpText

--- bin/zj-cli.js
+++ bin/zj-cli.js
@@ -4,15 +4,42 @@ const { program } = require('commander');
 const chalk = require('chalk');
 const pkg = require('../package.json');
 
-// 设置版本号
-program.version(pkg.version, '-v, --version');
+/**
+ * 设置CLI版本信息
+ * @param {string} version - 从package.json获取的版本号
+ * @param {string} flags - 版本查询标识(-v, --version)
+ * @param {string} description - 版本命令描述
+ */
+program.version(pkg.version, '-v, --version', 'display the current version number');
+
+/**
+ * 设置默认用法提示
+ * 当用户输入无效命令时显示
+ */
+program.usage('<command> [options]');
+
+/**
+ * 初始化命令配置
+ * @command init - 初始化新项目
+ * @description 初始化Vue或React项目
+ */
+program
+  .command('init')
+  .description('Initialize a new Vue or React project');
+
+/**
+ * 默认命令处理
+ * 当用户未输入任何命令时显示帮助信息
+ */
+program
+  .arguments('[command]')
+  .action((cmd) => {
+    if (!cmd) {
+      console.log(chalk.bold.blue('\n 🚀 Welcome to use ZJ-CLI \n'));
+      program.outputHelp(txt => txt);
+      console.log('\n run ' + chalk.green('zj-cli init') + chalk.gray(' - Initialize a new project \n'));
+    }
+  });
 
 // 解析命令行参数
 program.parse(process.argv);
-
-// 如果没有提供任何命令，显示帮助信息
-if (!process.argv.slice(2).length) {
-  console.log(chalk.bold.blue('\n 🚀 Welcom to use ZJ-CLI \n'));
-  program.outputHelp((txt) => chalk.cyan(txt));
-  console.log('\n run ' + chalk.green('zj-cli init') + chalk.gray(' - Initialize a new project \n'));
-}
